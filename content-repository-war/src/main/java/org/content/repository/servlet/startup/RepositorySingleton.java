959222bfc5b9348e76783eeef2e56352e63378f2
stephen.sill.2nd <stephen.sill.2nd@gmail.com>
2011-02-17 01:34:10 +0000

    Added a few comments

--- content-repository-war/src/main/java/org/content/repository/servlet/startup/RepositorySingleton.java
+++ content-repository-war/src/main/java/org/content/repository/servlet/startup/RepositorySingleton.java
@@ -1,27 +1,27 @@
 package org.content.repository.servlet.startup;
 
-//import org.scapdev.content.core.ContentRepository;
+import org.scapdev.content.core.ContentRepository;
 
 public enum RepositorySingleton 
 {
 	INSTANCE;
 	
-//	private ContentRepository repo;
+	private ContentRepository repo;
 	
 	private RepositorySingleton()
 	{
 		try
 		{
-//			repo = new ContentRepository(ContentRepositoryStartupServlet.class.getClassLoader());			
+			repo = new ContentRepository(ContentRepositoryStartupServlet.class.getClassLoader());			
 		}
 		catch(Exception e)
 		{
-			
+			System.out.println("Caught exception trying to initialize content repository: " + e.getMessage());
 		}
 	}
 	
 	public void loadLocalContent()
 	{
 		
-	}
+	}	
 }
