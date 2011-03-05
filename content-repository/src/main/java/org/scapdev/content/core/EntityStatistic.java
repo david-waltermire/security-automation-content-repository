f9ac08cafdf7b70a44ac5add22dbb0013d288212
stephen.sill.2nd <stephen.sill.2nd@gmail.com>
2011-03-05 01:02:36 +0000

    Stubbing out some methods and interfaces for getting statistics from the repository.  Checking in code Dave W. helped me with.

---
+++ content-repository/src/main/java/org/scapdev/content/core/EntityStatistic.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * The MIT License
+ * 
+ * Copyright (c) 2011 ssill2
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+package org.scapdev.content.core;
+
+import java.util.Map;
+
+import org.scapdev.content.model.EntityInfo;
+
+public interface EntityStatistic 
+{
+	/**
+	 * Get the EntityInfo object this statistic is for.
+	 * 
+	 * @return EntityInfo
+	 */
+	EntityInfo getEntityInfo();
+	
+	/**
+	 * Get the detailed relationship statistics for each entity instance of this
+	 * EntityInfo object.
+	 * 
+	 * @return Map<String, RelationshipStatistic>
+	 */
+	Map<String, RelationshipStatistic> getRelationshipInfoStatistics();
+	
+	/**
+	 * Get the number of Entity instances for this EntityInfo object.
+	 * @return int
+	 */
+	int getCount();
+}
---
+++ content-repository/src/main/java/org/scapdev/content/core/RelationshipStatistic.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * The MIT License
+ * 
+ * Copyright (c) 2011 ssill2
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+package org.scapdev.content.core;
+
+import org.scapdev.content.model.RelationshipInfo;
+
+public interface RelationshipStatistic
+{
+	/**
+	 * Get the RelationshipInfo object this statistic is for.
+	 * 
+	 * @return RelationshipInfo
+	 */
+	RelationshipInfo getRelationshipInfo();
+	
+	/**
+	 * Get the number of Relationship instances for this RelationshipInfo object.
+	 * @return
+	 */
+	int getCount();
+}
