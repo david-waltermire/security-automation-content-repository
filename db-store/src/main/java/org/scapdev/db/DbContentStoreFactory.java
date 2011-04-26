21da9ff91bd5a6db882422c2d3b58dce6954ec64
glenn.strickland@gmail.com <glenn.strickland@gmail.com@c2d1f623-6de7-8a39-a0c5-8c3d7efa00b1>
2011-04-26 22:25:29 +0000

    ContentStore implementation that stores entities in an SQL Server database.

---
+++ db-store/src/main/java/org/scapdev/db/DbContentStoreFactory.java
@@ -0,0 +1,19 @@
+package org.scapdev.db;
+
+import org.scapdev.content.core.persistence.hybrid.ContentStore;
+import org.scapdev.content.core.persistence.hybrid.ContentStoreFactory;
+
+	
+public class DbContentStoreFactory implements ContentStoreFactory {
+	
+	static DbContentStore INSTANCE = null;
+
+	public ContentStore newContentStore() {
+		if (INSTANCE == null) {
+			INSTANCE = new DbContentStore();
+		}
+		return INSTANCE;
+	}
+
+}
+
