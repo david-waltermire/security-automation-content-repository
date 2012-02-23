e199fddc271ab6ad32a1f5761c4e0108ff1eed88
davewaltermire <davewaltermire@gmail.com>
2012-02-23 22:49:54 +0000

    + Changed the return type of DataExtractingContentHandler#getEntities to List which preserves the depth first ordering semantics if a traditional iterator is used.

--- shredder/src/main/java/gov/nist/scap/content/shredder/parser/DataExtractingContentHandler.java
+++ shredder/src/main/java/gov/nist/scap/content/shredder/parser/DataExtractingContentHandler.java
@@ -10,11 +10,12 @@ import gov.nist.scap.content.shredder.model.IMutableEntity;
 import gov.nist.scap.content.shredder.rules.KeyedRelationshipInfo;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 
 import org.apache.log4j.Logger;
 import org.apache.xmlbeans.XmlCursor;
@@ -31,10 +32,12 @@ public class DataExtractingContentHandler implements ContentHandler, IContainerV
 	public DataExtractingContentHandler() {
 	}
 
-	public Queue<? extends IEntity<?>> getEntities() {
+	/**
+	 * @return a depth first ordered list of processed entities
+	 */
+	public List<? extends IEntity<?>> getEntities() {
 		process();
-		// TODO: make the queue unmodifiable
-		return entities;
+		return Collections.unmodifiableList(entities);
 	}
 
 	public void process() {
