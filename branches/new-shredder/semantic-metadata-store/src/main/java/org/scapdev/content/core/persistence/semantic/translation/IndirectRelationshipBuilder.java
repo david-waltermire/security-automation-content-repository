309d3f44fa261fd3d0d8e0e3c8a0ffa25d5a9023
adam.halbardier <adam.halbardier@c2d1f623-6de7-8a39-a0c5-8c3d7efa00b1>
2012-03-13 15:48:12 +0000

    Adding semantic project

---
+++ branches/new-shredder/semantic-metadata-store/src/main/java/org/scapdev/content/core/persistence/semantic/translation/IndirectRelationshipBuilder.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * The MIT License
+ * 
+ * Copyright (c) 2011 Paul Cichonski
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+package org.scapdev.content.core.persistence.semantic.translation;
+
+import gov.nist.scap.content.model.DefaultIndirectRelationship;
+import gov.nist.scap.content.model.IIndirectRelationship;
+import gov.nist.scap.content.model.definitions.IExternalIdentifier;
+import gov.nist.scap.content.model.definitions.IIndirectRelationshipDefinition;
+import gov.nist.scap.content.model.definitions.collection.IMetadataModel;
+
+/**
+ * Builder for constructing IndirectRelationships
+ *
+ */
+class IndirectRelationshipBuilder {
+	private IIndirectRelationshipDefinition relationshipDefinition;
+	private String externalIdValue;
+	private String externalIdType;
+	
+	IndirectRelationshipBuilder() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	void setExternalIdValue(String externalIdValue) {
+		this.externalIdValue = externalIdValue;
+	}
+	
+	void setIndirectRelationshipInfo(IIndirectRelationshipDefinition relationshipDefinition) {
+		this.relationshipDefinition = relationshipDefinition;
+	}
+	
+	void setExternalIdType(String externalIdType) {
+		this.externalIdType = externalIdType;
+	}
+
+	/**
+	 * Will build an instance of an indirect relationship. NOTE: the client must
+	 * add the newly created relationship to the owning entity directly after
+	 * calling this method.
+	 * 
+	 * @param model
+	 * @param entity
+	 *            - the owningEntity of the relationship
+	 * @return
+	 */
+	IIndirectRelationship build(IMetadataModel model, RebuiltEntity<?> entity){
+		if (relationshipDefinition == null || externalIdValue == null || externalIdType == null){
+			throw new IncompleteBuildStateException("Not all values are populated");
+		}
+		
+		IExternalIdentifier externalIdentifier = model.getExternalIdentifierInfoById(externalIdType);
+		IIndirectRelationship rel = new DefaultIndirectRelationship(relationshipDefinition, entity, externalIdentifier, externalIdValue);
+		
+		return rel;
+	}
+}
---
+++ branches/new-shredder/semantic-metadata-store/src/main/java/org/scapdev/content/core/persistence/semantic/translation/IndirectRelationshipStatementManager.java
@@ -0,0 +1,178 @@
+/*******************************************************************************
+ * The MIT License
+ * 
+ * Copyright (c) 2011 Paul Cichonski
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+package org.scapdev.content.core.persistence.semantic.translation;
+
+import gov.nist.scap.content.model.IIndirectRelationship;
+import gov.nist.scap.content.model.definitions.IIndirectRelationshipDefinition;
+import gov.nist.scap.content.model.definitions.collection.IMetadataModel;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.scapdev.content.core.persistence.semantic.MetaDataOntology;
+
+/**
+ * <p>A manager to coordinate multiple IndirectRelationshipBuilders working in
+ * parallel to build up all IndirectRelationships originating from a specific
+ * entity.</p>
+ * 
+ * @see IndirectRelationshipBuilder
+ */
+class IndirectRelationshipStatementManager implements RegenerationStatementManager {
+	private MetaDataOntology ontology;
+	
+	//should be okay to store at class level since this should be created/destroyed within a single instance
+	private IMetadataModel model;
+	
+	//all IDs of indirectRelationships
+	private Collection<String> indirectRelationshipIds;
+	
+	// key = boundary_boject_id....this map is what this class builds
+	private Map<String, IndirectRelationshipBuilder> indirectRelationships = new HashMap<String, IndirectRelationshipBuilder>();
+	
+	
+	IndirectRelationshipStatementManager(MetaDataOntology ontology, IMetadataModel model) {
+		this.ontology = ontology;
+		this.model = model;
+		this.indirectRelationshipIds = model.getIndirectRelationshipIds();
+	}
+	
+	/**
+	 * Scans triple and processes it if it is relevant to indirectRelationships
+	 * @param statement
+	 * @return true if triple was processed in some way, false if it was just ignored.
+	 */
+	public boolean scan(Statement statement){
+		URI predicate = statement.getPredicate();
+		if (indirectRelationshipIds.contains(predicate.stringValue())){
+			// a triple containing an indirect relationship predicate found
+			populateIndirectRelationshipInfo(model, statement);
+			return true;
+		}
+		if (predicate.equals(ontology.HAS_BOUNDARY_OBJECT_TYPE.URI)){
+			populateIndirectRelationshipExternalIdType(model, statement);
+			return true;
+		}
+		if (predicate.equals(ontology.HAS_BOUNDARY_OBJECT_VALUE.URI)){
+			populateIndirectRelationshipExternalIdValue(model, statement);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * <p>
+	 * Called after all triples are processed to populate re-constituted entity
+	 * with the IndirectRelationships found in the graph.
+	 * </p>
+	 * 
+	 * @param entity
+	 *            - to populate.
+	 */
+	public void populateEntity(RebuiltEntity<?> entity){
+		for (IndirectRelationshipBuilder indirectRelBuilder : indirectRelationships.values()){
+			IIndirectRelationship indirectRelationship = indirectRelBuilder.build(model, entity);
+			entity.addIndirectRelationship(indirectRelationship);
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Helper method to populate RelatioshipInfo based on the predicate found
+	 * within the triple. Assumes Statement passed in contains a predicate that
+	 * is a subProperty of HAS_INDIRECT_RELATIONSHIP.
+	 * </p>
+	 * 
+	 * @param model
+	 * @param statement
+	 * 
+	 * @see RelationshipInfo
+	 */
+	private void populateIndirectRelationshipInfo(IMetadataModel model, Statement statement){
+		// hit on an indirectRelationship of some type
+		String indirectRelationshipId = statement.getPredicate().stringValue();
+		String boundaryObjectURI = statement.getObject().stringValue();
+		IndirectRelationshipBuilder indirectRelBuilder = indirectRelationships.get(boundaryObjectURI);
+		if (indirectRelBuilder == null){
+			indirectRelBuilder = new IndirectRelationshipBuilder();
+			indirectRelBuilder.setIndirectRelationshipInfo((IIndirectRelationshipDefinition)model.getRelationshipInfoById(indirectRelationshipId));
+			indirectRelationships.put(boundaryObjectURI, indirectRelBuilder);
+		} else {
+			// can't be sure this was set before
+			indirectRelBuilder.setIndirectRelationshipInfo((IIndirectRelationshipDefinition)model.getRelationshipInfoById(indirectRelationshipId));
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Helper method to populate ExternalIdType based on the provided triple .
+	 * Assumes Statement passed in contains a HAS_BOUNDARY_OBJECT_TYPE predicate.
+	 * </p>
+	 * 
+	 * @param model
+	 * @param statement
+	 * 
+	 */
+	private void populateIndirectRelationshipExternalIdType(IMetadataModel model, Statement statement){
+		String boundaryObjectURI = statement.getSubject().stringValue();
+		String boundaryObjectType = statement.getObject().stringValue();
+		IndirectRelationshipBuilder indirectRelBuilder = indirectRelationships.get(boundaryObjectURI);
+		if (indirectRelBuilder == null){
+			indirectRelBuilder = new IndirectRelationshipBuilder();
+			indirectRelBuilder.setExternalIdType(boundaryObjectType);
+			indirectRelationships.put(boundaryObjectURI, indirectRelBuilder);
+		} else {
+			// can't be sure this was set before
+			indirectRelBuilder.setExternalIdType(boundaryObjectType);
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Helper method to populate ExternalIdValue based on the provided triple .
+	 * Assumes Statement passed in contains a HAS_BOUNDARY_OBJECT_VALUE predicate.
+	 * </p>
+	 * 
+	 * @param model
+	 * @param statement
+	 * 
+	 */
+	private void populateIndirectRelationshipExternalIdValue(IMetadataModel model, Statement statement){
+		String boundaryObjectURI = statement.getSubject().stringValue();
+		String boundaryObjectValue = statement.getObject().stringValue();
+		IndirectRelationshipBuilder indirectRelBuilder = indirectRelationships.get(boundaryObjectURI);
+		if (indirectRelBuilder == null){
+			indirectRelBuilder = new IndirectRelationshipBuilder();
+			indirectRelBuilder.setExternalIdValue(boundaryObjectValue);
+			indirectRelationships.put(boundaryObjectURI, indirectRelBuilder);
+		} else {
+			// can't be sure this was set before
+			indirectRelBuilder.setExternalIdValue(boundaryObjectValue);
+		}
+	}
+	
+}
