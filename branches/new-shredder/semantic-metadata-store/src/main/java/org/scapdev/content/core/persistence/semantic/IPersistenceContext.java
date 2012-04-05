b7781d535897befdbc1786c24953683c6af3ac02
adam.halbardier@gmail.com <adam.halbardier@gmail.com@c2d1f623-6de7-8a39-a0c5-8c3d7efa00b1>
2012-04-05 18:40:22 +0000

    Refactoring and updating java docs

--- branches/new-shredder/semantic-metadata-store/src/main/java/org/scapdev/content/core/persistence/semantic/IPersistenceContext.java
+++ branches/new-shredder/semantic-metadata-store/src/main/java/org/scapdev/content/core/persistence/semantic/IPersistenceContext.java
@@ -5,8 +5,20 @@ import org.scapdev.content.core.persistence.hybrid.ContentRetrieverFactory;
 
 public interface IPersistenceContext {
 
+    /**
+     * Get the repository within context
+     * @return the repository
+     */
     Repository getRepository();
+    /**
+     * Get the ontology within context
+     * @return the ontology
+     */
     MetaDataOntology getOntology();
+    /**
+     * Get the content retriever factory within context
+     * @return the content retriever factory
+     */
     ContentRetrieverFactory getContentRetrieverFactory();
     
 }
--- branches/new-shredder/semantic-metadata-store/src/main/java/org/scapdev/content/core/persistence/semantic/entity/IKeyedInternalBuilder.java
+++
@@ -1,5 +0,0 @@
-package org.scapdev.content.core.persistence.semantic.entity;
-
-public interface IKeyedInternalBuilder extends IInternalBuilder {
-
-}
