21d5ad05e47944f3765a37996399cda793173240
adam.halbardier@gmail.com <adam.halbardier@gmail.com@c2d1f623-6de7-8a39-a0c5-8c3d7efa00b1>
2012-04-05 19:21:11 +0000

    Updating javadocs

--- branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/translation/EntityMetadataMap.java
+++ branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/translation/EntityMetadataMap.java
@@ -6,6 +6,16 @@ import org.openrdf.model.URI;
 
 public interface EntityMetadataMap {
 
+    /**
+     * get the resource URI of an entity
+     * @param entity the entity of interest
+     * @return the URI of the entity
+     */
     URI getResourceURI(IEntity<?> entity);
+    /**
+     * get the content ID of an entity
+     * @param entity the entity of interest
+     * @return the content ID of the entity
+     */
     String getContentId(IEntity<?> entity);
 }
