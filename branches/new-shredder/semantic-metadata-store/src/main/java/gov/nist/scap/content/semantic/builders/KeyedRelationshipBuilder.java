d93880cbdf8c7db6e33991d2eda34ba3fc658ea2
adam.halbardier@gmail.com <adam.halbardier@gmail.com@c2d1f623-6de7-8a39-a0c5-8c3d7efa00b1>
2012-04-06 19:39:03 +0000

    Adding javadocs, and making cache thread safe

--- branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/builders/KeyedRelationshipBuilder.java
+++ branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/builders/KeyedRelationshipBuilder.java
@@ -36,53 +36,64 @@ import gov.nist.scap.content.semantic.exceptions.IncompleteBuildStateException;
 
 import org.openrdf.repository.RepositoryException;
 
+/**
+ * A builder to create keyed relationships for statements about an entity
+ * 
+ * @author Adam Halbardier
+ */
 public class KeyedRelationshipBuilder {
-	private IKeyedRelationshipDefinition keyedRelationshipInfo;
-	
-	private IKey relatedEntityKey;
-	private IPersistenceContext ipc;
-	
-	/**
-	 * default constructor
-	 * @param ipc the persistence context
-	 */
-	public KeyedRelationshipBuilder(IPersistenceContext ipc) {
-	    this.ipc = ipc;
-	}
-	
-	/**
-	 * set the definition of the relationship
-	 * @param keyedRelationshipInfo the definition of the relationship
-	 */
-	public void setKeyedRelationshipInfo(IKeyedRelationshipDefinition keyedRelationshipInfo) {
-		this.keyedRelationshipInfo = keyedRelationshipInfo;
-	}
-	
-	/**
-	 * set the target entity key
-	 * @param relatedEntityKey the target entity key
-	 */
-	public void setRelatedEntityKey(IKey relatedEntityKey) {
-		this.relatedEntityKey = relatedEntityKey;
-	}
+    private IKeyedRelationshipDefinition keyedRelationshipInfo;
 
+    private IKey relatedEntityKey;
+    private IPersistenceContext ipc;
 
-	
-	
-	/**
-	 * Will build an instance of an keyed relationship.
-	 * 
-	 * @param model the metadata mode
-	 * @return the relationship that must be added to the owning entity
-	 */
-	public IKeyedRelationship build(IMetadataModel model){
-		if (keyedRelationshipInfo == null || relatedEntityKey == null){
-			throw new IncompleteBuildStateException("Not all values are populated");
-		}
-		try {
-            return new DefaultKeyedRelationship(keyedRelationshipInfo, new KeyedEntityProxy<IKeyedEntityDefinition, IKeyedEntity<IKeyedEntityDefinition>>(ipc, relatedEntityKey));
+    /**
+     * default constructor
+     * 
+     * @param ipc the persistence context
+     */
+    public KeyedRelationshipBuilder(IPersistenceContext ipc) {
+        this.ipc = ipc;
+    }
+
+    /**
+     * set the definition of the relationship
+     * 
+     * @param keyedRelationshipInfo the definition of the relationship
+     */
+    public void setKeyedRelationshipInfo(
+            IKeyedRelationshipDefinition keyedRelationshipInfo) {
+        this.keyedRelationshipInfo = keyedRelationshipInfo;
+    }
+
+    /**
+     * set the target entity key
+     * 
+     * @param relatedEntityKey the target entity key
+     */
+    public void setRelatedEntityKey(IKey relatedEntityKey) {
+        this.relatedEntityKey = relatedEntityKey;
+    }
+
+    /**
+     * Will build an instance of an keyed relationship.
+     * 
+     * @param model the metadata mode
+     * @return the relationship that must be added to the owning entity
+     */
+    public IKeyedRelationship build(IMetadataModel model) {
+        if (keyedRelationshipInfo == null || relatedEntityKey == null) {
+            throw new IncompleteBuildStateException(
+                "Not all values are populated");
+        }
+        try {
+            return new DefaultKeyedRelationship(
+                keyedRelationshipInfo,
+                new KeyedEntityProxy<IKeyedEntityDefinition, IKeyedEntity<IKeyedEntityDefinition>>(
+                    ipc,
+                    relatedEntityKey));
         } catch (RepositoryException e) {
             throw new RuntimeException(e);
         }
-	}
+    }
 }
--- branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/entity/EntityBuilder.java
+++ branches/new-shredder/semantic-metadata-store/src/main/java/gov/nist/scap/content/semantic/entity/EntityBuilder.java
@@ -24,144 +24,178 @@ import java.util.Map;
 
 import org.scapdev.content.core.persistence.hybrid.ContentRetriever;
 
+/**
+ * A builder to construct and entity
+ * 
+ * @author Adam Halbardier
+ */
 public class EntityBuilder {
-	private ContentRetriever contentRetriever;
-	private IKey key;
-	private String versionValue;
-	private IContainer<?> parent;
-	private final List<IRelationship<?>> relationships = new LinkedList<IRelationship<?>>();
-	private final Map<IPropertyDefinition, List<String>> properties = new HashMap<IPropertyDefinition, List<String>>();
-	private IEntityDefinition definition;
+    private ContentRetriever contentRetriever;
+    private IKey key;
+    private String versionValue;
+    private IContainer<?> parent;
+    private final List<IRelationship<?>> relationships =
+        new LinkedList<IRelationship<?>>();
+    private final Map<IPropertyDefinition, List<String>> properties =
+        new HashMap<IPropertyDefinition, List<String>>();
+    private IEntityDefinition definition;
 
-	/**
-	 * set the entity definition
-	 * @param definition the entity definition
-	 */
-	public void setEntityDefinition(IEntityDefinition definition) {
-		this.definition = definition;
-	}
+    /**
+     * set the entity definition
+     * 
+     * @param definition the entity definition
+     */
+    public void setEntityDefinition(IEntityDefinition definition) {
+        this.definition = definition;
+    }
 
-	/**
-	 * set the content retriever
-	 * @param contentRetriever the content retriever
-	 */
-	public void setContentRetriever(ContentRetriever contentRetriever) {
-		if (this.contentRetriever != null) {
-			throw new IllegalStateException("contentRetriever has already been set");
-		}
-		this.contentRetriever = contentRetriever;
-	}
-
-	/**
-	 * set the key
-	 * @param key the key
-	 */
-	public void setKey(IKey key) {
-		if (this.key != null) {
-			throw new IllegalStateException("key has already been set");
-		}
-		this.key = key;
-	}
-	
-	/**
-	 * set the version value
-	 * @param version the version value
-	 */
-	public void setVersionValue(String version) {
-        if (this.versionValue != null) {
-            throw new IllegalStateException("version value has already been set");
+    /**
+     * set the content retriever
+     * 
+     * @param contentRetriever the content retriever
+     */
+    public void setContentRetriever(ContentRetriever contentRetriever) {
+        if (this.contentRetriever != null) {
+            throw new IllegalStateException(
+                "contentRetriever has already been set");
         }
-	    this.versionValue = version;
-	}
+        this.contentRetriever = contentRetriever;
+    }
 
-	/**
-	 * set the parent entity
-	 * @param parent the parent entity
-	 */
-	public void setParent(IContainer<?> parent) {
-		if (this.parent != null) {
-			throw new IllegalStateException("parent has already been set");
-		}
-		this.parent = parent;
-	}
+    /**
+     * set the key
+     * 
+     * @param key the key
+     */
+    public void setKey(IKey key) {
+        if (this.key != null) {
+            throw new IllegalStateException("key has already been set");
+        }
+        this.key = key;
+    }
 
-	/**
-	 * add relationship to entity
-	 * @param relationship relationship to add
-	 */
-	public void addRelationship(IRelationship<?> relationship) {
-		this.relationships.add(relationship);
-	}
+    /**
+     * set the version value
+     * 
+     * @param version the version value
+     */
+    public void setVersionValue(String version) {
+        if (this.versionValue != null) {
+            throw new IllegalStateException(
+                "version value has already been set");
+        }
+        this.versionValue = version;
+    }
 
-	/**
-	 * add property to entity
-	 * @param definition definition of property
-	 * @param value value of property
-	 */
-	public void addProperty(IPropertyDefinition definition, String value) {
-		if (definition == null) {
-			throw new NullPointerException("definition");
-		}
-		if (value == null) {
-			throw new NullPointerException("value");
-		}
-		List<String> result = properties.get(definition);
-		if (result == null) {
-			result = new LinkedList<String>();
-			properties.put(definition, result);
-		}
-		result.add(value);
-	}
+    /**
+     * set the parent entity
+     * 
+     * @param parent the parent entity
+     */
+    public void setParent(IContainer<?> parent) {
+        if (this.parent != null) {
+            throw new IllegalStateException("parent has already been set");
+        }
+        this.parent = parent;
+    }
 
-	/**
-	 * build the entity
-	 * @param <T> the result type
-	 * @return the built entity
-	 */
-	public <T extends IEntity<?>> T build() {
-	    try {
+    /**
+     * add relationship to entity
+     * 
+     * @param relationship relationship to add
+     */
+    public void addRelationship(IRelationship<?> relationship) {
+        this.relationships.add(relationship);
+    }
+
+    /**
+     * add property to entity
+     * 
+     * @param definition definition of property
+     * @param value value of property
+     */
+    public void addProperty(IPropertyDefinition definition, String value) {
+        if (definition == null) {
+            throw new NullPointerException("definition");
+        }
+        if (value == null) {
+            throw new NullPointerException("value");
+        }
+        List<String> result = properties.get(definition);
+        if (result == null) {
+            result = new LinkedList<String>();
+            properties.put(definition, result);
+        }
+        result.add(value);
+    }
+
+    /**
+     * build the entity
+     * 
+     * @param <T> the result type
+     * @return the built entity
+     */
+    public <T extends IEntity<?>> T build() {
+        try {
             return this.definition.accept(new EntityDefinitionVisitor<T>());
         } catch (ProcessingException e) {
             throw new RuntimeException(e);
         }
-	}
+    }
 
-	private class EntityDefinitionVisitor<T extends IEntity<?>> implements IEntityDefinitionVisitor<T> {
+    private class EntityDefinitionVisitor<T extends IEntity<?>> implements
+            IEntityDefinitionVisitor<T> {
 
-		public T visit(IGeneratedDocumentDefinition definition) {
-		    DefaultGeneratedDocument dgd = new DefaultGeneratedDocument(definition, contentRetriever, parent);
-		    setCommonInfo(dgd);
-	        @SuppressWarnings("unchecked")
-		    T retVal = (T)dgd;
-		    return retVal;
-		}
+        public T visit(IGeneratedDocumentDefinition definition) {
+            DefaultGeneratedDocument dgd =
+                new DefaultGeneratedDocument(
+                    definition,
+                    contentRetriever,
+                    parent);
+            setCommonInfo(dgd);
+            @SuppressWarnings("unchecked")
+            T retVal = (T)dgd;
+            return retVal;
+        }
 
-		public T visit(IKeyedDocumentDefinition definition) {
-		    DefaultKeyedDocument dkd = new DefaultKeyedDocument(definition, key, contentRetriever, parent);
-		    setCommonInfo(dkd);
-	        @SuppressWarnings("unchecked")
-		    T retVal = (T)dkd;
-	        return retVal;
-		}
+        public T visit(IKeyedDocumentDefinition definition) {
+            DefaultKeyedDocument dkd =
+                new DefaultKeyedDocument(
+                    definition,
+                    key,
+                    contentRetriever,
+                    parent);
+            setCommonInfo(dkd);
+            @SuppressWarnings("unchecked")
+            T retVal = (T)dkd;
+            return retVal;
+        }
 
-		public T visit(IContentNodeDefinition definition) {
-			DefaultContentNode dcn = new DefaultContentNode(definition, key, contentRetriever, parent);
-			setCommonInfo(dcn);
-		     @SuppressWarnings("unchecked")
-			 T retVal = (T)dcn;
-		     return retVal;
-		}
-		
-		private void setCommonInfo(IMutableEntity<?> ime) {
-	        for (IRelationship<?> relationship : relationships) {
-	            ime.addRelationship(relationship);
-	        }
-	        for (Map.Entry<IPropertyDefinition, List<String>> entry : properties.entrySet()) {
-	            ime.addProperty(entry.getKey().getId(), entry.getValue());
-	        }
-	        if( definition.getVersionDefinition() != null ) {
-	            ime.setVersion(new DefaultVersion(definition.getVersionDefinition(), versionValue));
-	        }
-		}
-	}
+        public T visit(IContentNodeDefinition definition) {
+            DefaultContentNode dcn =
+                new DefaultContentNode(
+                    definition,
+                    key,
+                    contentRetriever,
+                    parent);
+            setCommonInfo(dcn);
+            @SuppressWarnings("unchecked")
+            T retVal = (T)dcn;
+            return retVal;
+        }
+
+        private void setCommonInfo(IMutableEntity<?> ime) {
+            for (IRelationship<?> relationship : relationships) {
+                ime.addRelationship(relationship);
+            }
+            for (Map.Entry<IPropertyDefinition, List<String>> entry : properties.entrySet()) {
+                ime.addProperty(entry.getKey().getId(), entry.getValue());
+            }
+            if (definition.getVersionDefinition() != null) {
+                ime.setVersion(new DefaultVersion(
+                    definition.getVersionDefinition(),
+                    versionValue));
+            }
+        }
+    }
 }
